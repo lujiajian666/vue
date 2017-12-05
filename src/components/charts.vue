78c62c123cba75dc4d81d36758246a397e295de0
lujiajian <13794649942@163.com>
2017-12-05 22:47:21 +0800

    12-5

---
+++ src/components/charts.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="index">
+     <head-div select="two"></head-div>
+     <float-right-div></float-right-div>
+     <content-div></content-div>
+     <foot-div></foot-div>
+  </div>
+</template>
+
+<script>
+import head from '@/components/head';
+import floatRightDiv from '@/components/floatRightDiv';
+import contentDiv from "./content-div"
+import ContentDiv from "./content-div.vue";
+import footDiv from "./foot-div";
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: "data"
+    }
+  },
+  components:{
+      ContentDiv,
+      "head-div":head,
+      "float-right-div":floatRightDiv,
+      "content-div":contentDiv,
+      "foot-div":footDiv
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
