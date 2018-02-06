45e922c7a2782e7365a8b6282f4f871e73c07c6d
lujiajian <13794649942@163.com>
2018-02-07 05:53:12 +0800

    申请假期界面完成，逻辑未完成

--- src/components/background/VacationApply.vue
+++ src/components/background/VacationApply.vue
@@ -1,24 +1,42 @@
 <template>
     <div id="vacationApply">
         <el-collapse v-model="activeName" accordion>
-            <el-collapse-item title="一致性 Consistency" name="1">
-                <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-                <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+            <el-collapse-item title="请假日期" name="1">
+                <div class="block">
+                    <el-date-picker
+                            v-model="value7"
+                            type="daterange"
+                            align="right"
+                            unlink-panels
+                            range-separator="至"
+                            start-placeholder="开始日期"
+                            end-placeholder="结束日期"
+                            :picker-options="pickerOptions2">
+                    </el-date-picker>
+                </div>
             </el-collapse-item>
-            <el-collapse-item title="反馈 Feedback" name="2">
-                <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-                <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+            <el-collapse-item title="请假原因" name="2">
+                <div style="padding:10px 30px">
+                    <el-input
+                            type="textarea"
+                            :rows="10"
+                            placeholder="请输入内容"
+                            v-model="textarea">
+                    </el-input>
+                </div>
+
             </el-collapse-item>
-            <el-collapse-item title="效率 Efficiency" name="3">
-                <div>简化流程：设计简洁直观的操作流程；</div>
-                <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-                <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-            </el-collapse-item>
-            <el-collapse-item title="可控 Controllability" name="4">
-                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+            <el-collapse-item title="统计" name="3">
+                <div>你已经累计休假 10 天</div>
+                <div>年假剩余 0 天</div>
+                <el-switch
+                        v-model="value3"
+                        inactive-text="我已阅读">
+                </el-switch>
             </el-collapse-item>
         </el-collapse>
+        <br><br><br><br>
+        <el-button type="primary" :disabled="disable">确认申请</el-button>
     </div>
 </template>
 
@@ -26,7 +44,39 @@
     export default {
         data() {
             return {
-                activeName: '1'
+                disable:true,
+                value3: false,
+                textarea: '',
+                activeName: '1',
+                pickerOptions2: {
+                    shortcuts: [{
+                        text: '最近一周',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近一个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }, {
+                        text: '最近三个月',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    }]
+                },
+                value6: '',
+                value7: ''
             }
         },
         components:{
